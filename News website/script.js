96b7a645b90c5aa8ab76ba31f91d6e2b9a23278b
Lordnhell <87353930+Lordnhell@users.noreply.github.com>
2021-07-14 04:24:12 +0800

    Add files via upload

---
+++ News website/script.js
@@ -0,0 +1,29 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
+
+  function searchFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("sidebarSearch");
+    console.log(input)
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myMenu");
+    li = ul.getElementsByTagName("li");
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
